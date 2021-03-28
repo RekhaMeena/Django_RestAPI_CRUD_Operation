fe6060b335382c8a398c54f1047859d9e180376d
RekhaMeena <56825202+RekhaMeena@users.noreply.github.com>
2021-03-29 00:31:54 +0530

    languages/apps

---
+++ serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers 
+from languages.models import Language, Paradigm, Programmer
+ 
+ 
+class LanguageSerializer(serializers.HyperlinkedModelSerializer):
+ 
+    class Meta:
+        model = Language
+        fields = ('id', 'url', 'name', 'paradigm')
+
+class ParadigmSerializer(serializers.HyperlinkedModelSerializer):
+ 
+    class Meta:
+        model = Paradigm
+        fields = ('id', 'url', 'name')
+
+class ProgrammerSerializer(serializers.HyperlinkedModelSerializer):
+ 
+    class Meta:
+        model = Programmer
+        fields = ('id', 'url', 'name', 'languages')
